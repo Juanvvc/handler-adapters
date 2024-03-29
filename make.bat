13b92f04ca70144db0b0629e210d09d2fe85b406
Juan Vera del Campo <juanvvc@gmail.com>
2024-03-29 18:09:27 +0100

    Add handler body

--- make.bat
+++ make.bat
@@ -1,13 +1,21 @@
 set OPENSCAD="C:\Program Files\OpenSCAD\openscad.exe"
+mkdir output
 
-%OPENSCAD% -D base_r=12.5 -o adapter_25mm.png -q --autocenter handler_adapter.scad
-%OPENSCAD% -D base_r=12.5 -o adapter_25mm.stl -q --export-format binstl handler_adapter.scad
+%OPENSCAD% -D base_r=12.5 -o output\adapter_25mm.png -q --autocenter handler_adapter.scad
+%OPENSCAD% -D base_r=12.5 -o output\adapter_25mm.stl -q --export-format binstl handler_adapter.scad
 
-%OPENSCAD% -D base_r=16 -o adapter_32mm.png -q --autocenter handler_adapter.scad
-%OPENSCAD% -D base_r=16 -o adapter_32mm.stl -q --export-format binstl handler_adapter.scad
+%OPENSCAD% -D base_r=16 -o output\adapter_32mm.png -q --autocenter handler_adapter.scad
+%OPENSCAD% -D base_r=16 -o output\adapter_32mm.stl -q --export-format binstl handler_adapter.scad
 
-%OPENSCAD% -D base_r=25 -o adapter_50mm.png -q --autocenter handler_adapter.scad
-%OPENSCAD% -D base_r=25 -o adapter_50mm.stl -q --export-format binstl handler_adapter.scad
+%OPENSCAD% -D base_r=25 -o output\adapter_50mm.png -q --autocenter handler_adapter.scad
+%OPENSCAD% -D base_r=25 -o output\adapter_50mm.stl -q --export-format binstl handler_adapter.scad
 
-%OPENSCAD% -D base_r=15 -D base_w=25 -o adapter_30x50mm.png -q --autocenter handler_adapter.scad
-%OPENSCAD% -D base_r=15 -D base_w=25 -o adapter_30x50mm.stl -q --export-format binstl handler_adapter.scad
+%OPENSCAD% -D base_r=15 -D base_w=25 -o output\adapter_30x50mm.png -q --autocenter handler_adapter.scad
+%OPENSCAD% -D base_r=15 -D base_w=25 -o output\adapter_30x50mm.stl -q --export-format binstl handler_adapter.scad
+
+%OPENSCAD% -o output\handler.png -q --autocenter handler.scad
+%OPENSCAD% -o output\handler.stl -q --export-format binstl handler.scad
+
+
+%OPENSCAD% -o output\handler_base.png -q --autocenter handler_base.scad
+%OPENSCAD% -o output\handler_base.stl -q --export-format binstl handler_base.scad
